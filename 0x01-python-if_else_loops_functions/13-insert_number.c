bdd0530435eeeb657e2611139fde904ff6316b5b
Owl-3 <d.odira@alustudent.com>
2021-09-07 19:15:55 +0000

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,49 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
-/**
- * insert_node - inserts a node in a linked list
- * @head: double pointer to the head of the list
- * @number: value to be added to node
- * Return: the address of the new node, or NULL if it failed
- */
 
+/**
+ * insert_node - inserts node into sorted linked list
+ * @head: pointer to pointer to first node in linked list
+ * @number: the number to insert into linked list
+ * Return: the address of the new node or NULL if it fails
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-  listint_t *current, *temp, *new;
+  listint_t *current, *temp;
+  listint_t *new;
 
-  current = *head, temp = *head, new = NULL;
-
+  if (head == NULL)
+    return (NULL);
   new = malloc(sizeof(listint_t));
   if (new == NULL)
     return (NULL);
+  new->n = number;
+  new->next = NULL;
   if (*head == NULL)
     {
-      new->n = number;
-      new->next = NULL;
       *head = new;
       return (new);
     }
-  if (temp == NULL)
-    return (NULL);
-  if (temp->n >= number)
+  for (current = *head; current && current->next; current = current->next)
     {
-      new->n = number;
-      new->next = temp;
-      *head = new;
-      return (new);
-    }
-  while (current->next)
-    {
-      current = current->next;
-      if (current->n >= number)
+      if (number < current->n)
 	{
-	  new->n = number;
-	  new->next = current;
-	  temp->next = new;
+	  temp = *head;
+	  *head = new;
+	  new->next = temp;
+	  return (new->next);
+	}
+      else if (number < current->next->n)
+	{
+	  temp = current->next;
+	  current->next = new;
+	  new->next = temp;
 	  return (new);
 	}
-      else
-	temp = temp->next;
     }
-  new = add_nodeint_end(head, number);
-  return (new);
-}
+  current->next = new;
+  return (current->next);#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+			   /**
+			    * insert_node - inserts node into sorted linked list
+			    * @head: pointer to pointer to first node in linked list
+			    * @number: the number to insert into linked list
+			    * Return: the address of the new node or NULL if it fails
+			    */
+			   listint_t *insert_node(listint_t **head, int number)
+			   {
+			     listint_t *current, *temp;
+			     listint_t *new;
+
+			     if (head == NULL)
+			       return (NULL);
+			     new = malloc(sizeof(listint_t));
+			     if (new == NULL)
+			       return (NULL);
+			     new->n = number;
+			     new->next = NULL;
+			     if (*head == NULL)
+			       {
+				 *head = new;
+				 return (new);
+			       }
+			     for (current = *head; current && current->next; current = current->next)
+			       {
+				 if (number < current->n)
+				   {
+				     temp = *head;
+				     *head = new;
+				     new->next = temp;
+				     return (new->next);
+				   }
+				 else if (number < current->next->n)
+				   {
+				     temp = current->next;
+				     current->next = new;
+				     new->next = temp;
+				     return (new);
+				   }
+			       }
+			     current->next = new;
+			     return (current->next);
