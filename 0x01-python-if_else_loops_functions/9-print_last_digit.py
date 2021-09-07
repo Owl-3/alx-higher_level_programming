0428c2b1bda68188762d11daf0ac6a4e227d4665
Owl-3 <d.odira@alustudent.com>
2021-09-07 19:30:13 +0000

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,9 @@
-0;136;0c#!/usr/bin/python3
+#!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    num = number % 10
-    print("{}".format(num), end='')
-    return num 
+    if (number > 0):
+        i = number % 10
+        print("{}".format(i), end="")
+    else:
+        i = -number % 10
+        print("{}".format(i), end="")
+    return (i)
