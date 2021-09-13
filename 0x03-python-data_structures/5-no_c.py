4b6600f7cd896ffb53fc63f0bc8747ccc344b811
Owl-3 <d.odira@alustudent.com>
2021-09-13 18:08:33 +0200

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-                alt_string = ""
-
-                    i = 0
-
-                        while (i < len(my_string)):
-                                            if my_string[i] is not "c" and my_string[i] is not "C":
-                                                                    alt_string += my_string[i]
-                                                                            i = i + 1
-
-                                                                                my_string = alt_string
-                                                                                    return my_string
-                                                                            # the new string is equal to itself plus my_string at i so we do
-                                                                                # not reset the index of the string each loop.
-                                                                                    # we need to return my_string, the original string
-                                                                                    
+                    new_string = []
+                        for i in my_string:
+                                                if i != 'c' and i != 'C':
+                                                                            new_string.append(i)
+                                                                                new_string = "".join(new_string)
+                                                                                    return new_string
