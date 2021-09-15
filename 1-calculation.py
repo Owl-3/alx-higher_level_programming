3178498dac75dfa73ed4e6a9f89bb01f3361f112
Owl-3 <d.odira@alustudent.com>
2021-09-15 15:30:14 -0700

    1-calculation.py

--- 1-calculation.py
+++ 1-calculation.py
@@ -1,15 +1,60 @@
 #!/usr/bin/python3
 # 1-calculation.py
 
-
-if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
-    from calculator_1 import add, sub, mul, div
-
+def add(a, b):
+    """creating a calculator"""
     a = 10
     b = 5
+    return a+b
+add(1, 2)
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b))) 
+
+#!/usr/bin/python3
+# 1-calculation.py
+
+def add(a, b):
+    """creating a calculator"""
+    a = 10
+    b = 5
+    return a+b
+add(1, 2)
+
+#!/usr/bin/python3
+# 1-calculation.py
+
+def add(a, b):
+    """creating a calculator"""
+    a = 10
+    b = 5
+    return a+b
+add(1, 2)
+#!/usr/bin/python3
+# 1-calculation.py
+
+def add(a, b):
+    """creating a calculator"""
+    a = 10
+    b = 5
+    return a+b
+add(1, 2)
+
+def sub(a, b):
+    """creating a calculator"""
+    a = 10
+    b = 5
+    return a - b
+sub(1, 2)
+
+def mul(a, b):
+    """creating a calculator"""
+    a = 10
+    b = 5
+    return a * b
+mul(1, 2)
+
+def div(a, b):
+    """creating a calculator"""
+    a = 10
+    b = 5
+    return a / b
+div(1, 2) 
