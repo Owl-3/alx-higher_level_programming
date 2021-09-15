f47f61a2be308489c48d93670fa8bcfe5b5c515b
Owl-3 <d.odira@alustudent.com>
2021-09-15 15:45:49 -0700

    1-calculation.py

--- 1-calculation.py
+++ 1-calculation.py
@@ -1,62 +1,43 @@
 #!/usr/bin/python3
-# 1-calculation.py
-if __name__ == "__main__":
-    """Print the sum, difference, multiple and quotient of 10 and 5."""
-    from calculator_1 import add, sub, mul, div
 def add(a, b):
-    """creating a calculator"""
-    a = 10
-    b = 5
-    return a+b
-add(1, 2)
+    """My addition function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
-
-#!/usr/bin/python3
-# 1-calculation.py
-
-def add(a, b):
-    """creating a calculator"""
-    a = 10
-    b = 5
-    return a+b
-add(1, 2)
-
-#!/usr/bin/python3
-# 1-calculation.py
-
-def add(a, b):
-    """creating a calculator"""
-    a = 10
-    b = 5
-    return a+b
-add(1, 2)
-#!/usr/bin/python3
-# 1-calculation.py
-
-def add(a, b):
-    """creating a calculator"""
-    a = 10
-    b = 5
-    return a+b
-add(1, 2)
 
 def sub(a, b):
-    """creating a calculator"""
-    a = 10
-    b = 5
-    return a - b
-sub(1, 2)
+    """My subtraction function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
 
 def mul(a, b):
-    """creating a calculator"""
-    a = 10
-    b = 5
-    return a * b
-mul(1, 2)
+    """My multiplication function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
 
 def div(a, b):
-    """creating a calculator"""
-    a = 10
-    b = 5
-    return a / b
-div(1, 2) 
+    """My division function
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b) 
