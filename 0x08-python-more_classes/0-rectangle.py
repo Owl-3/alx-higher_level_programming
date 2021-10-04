514ff7265ebe9c444e4a1b5f3ab229648c82313f
Owl-3 <d.odira@alustudent.com>
2021-10-04 16:00:07 +0000

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""0-rectangle module"""
+# 0-rectangle.py
+"""Defines a Rectangle class.
+"""
 
 
-class Rectangle():
-    pass
+class Rectangle:
+    """Represent a rectangle.
+    """
+    pass 
