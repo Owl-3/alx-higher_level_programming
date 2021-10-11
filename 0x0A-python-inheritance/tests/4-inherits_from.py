15e2fa6e2d9fff2328e7559936e64768cf524409
Owl-3 <d.odira@alustudent.com>
2021-10-11 21:02:15 +0000

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/tests/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""4-inherits_from module"""
+
+
+def inherits_from(obj, a_class):
+    """inherits_from function to determine if the type of obj is inherited"""
+    if type(obj) != a_class:
+        return isinstance(obj, a_class)
+    return False 
