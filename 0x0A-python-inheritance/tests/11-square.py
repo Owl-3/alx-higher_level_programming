91886a54745e1c353a0c3093e5755e3bf00285d1
Owl-3 <d.odira@alustudent.com>
2021-10-11 20:58:35 +0000

    11-square.py

---
+++ 0x0A-python-inheritance/tests/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""11-square module"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square Class"""
+
+    def __init__(self, size):
+        """__init__ magic method"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area method"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """__str__ magic method"""
+        return "[Square] {}/{}".format(self.__size, self.__size) 
