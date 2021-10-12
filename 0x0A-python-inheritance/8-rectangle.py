41d64b5ec7c097a83a8a197322543becdb44639b
Owl-3 <d.odira@alustudent.com>
2021-10-12 09:07:42 +0000

    8-rectangle.py

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""8-rectangle module"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class"""
+
+    def __init__(self, width, height):
+        """init magic method initialization procedure"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height 
