d5934ce264ac6465c4ae6fbe78f7f4e0b105b016
Owl-3 <d.odira@alustudent.com>
2021-10-12 09:06:51 +0000

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""7-base_geometry module"""
+
+
+class BaseGeometry(object):
+    """BaseGeometry Class"""
+    def __init__(self):
+        """init magic method"""
+        pass
+
+    def area(self):
+        """area method raises exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """int validator method validates value as integer"""
+        if isinstance(name, str):
+            if type(value) != int:
+                raise TypeError("{} must be an integer".format(str(name)))
+            elif value <= 0:
+                raise ValueError("{} must be greater than 0".format(str(name))) x
